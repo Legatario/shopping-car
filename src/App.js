907b5699c66974b737aec702227bd3a00b964502
Legatario <l.phillipe.d@gmail.com>
2024-04-25 17:32:06 -0300

    criando lista de produtos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Products from './components/Produucts/Prodectjs';
 
 function App() {
   return (
     <div>
       <Header />
+      <Products />
     </div>
   );
 }
---
+++ src/components/Produucts/Prodectjs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './products.css';
+
+function Products(){
+  return(
+    <section className="products container">
+      prosasas
+    </section>
+  );
+}
+
+export default Products;
