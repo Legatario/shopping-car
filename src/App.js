9f4fbc1d70704e58cf8ba2fc38cea4bd2bbbda93
Legatario <l.phillipe.d@gmail.com>
2024-04-26 17:10:53 -0300

    criando footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import Products from './components/Products/Prodectjs';
+import Footer from './components/Footer/Footer';
+
 
 function App() {
   return (
     <div>
       <Header />
       <Products />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer (){
+  return(
+    <footer>
+      <p>MKS sistemas ©️ Todos os direitos reservados</p>
+    </footer>
+  );
+}
+
+export default Footer;
