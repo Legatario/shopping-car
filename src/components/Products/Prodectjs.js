6a6853e096c98ab8e868e2581b7b033f85eb7b42
Legatario <l.phillipe.d@gmail.com>
2024-04-28 14:27:17 -0300

    criando loading

--- src/components/Products/Prodectjs.js
+++ src/components/Products/Prodectjs.js
@@ -2,29 +2,34 @@ import React, {useState, useEffect} from 'react';
 import './products.css';
 import fetchProduts from '../../api/fechProduts';
 import ProductsCard from '../ProductsCard/ProductsCard';
+import Loading from '../Loading/Loading';
 
 function Products(){
 
   const [products, setProducts] = useState([]);
+  const [loading, setloading] = useState(true);
 
   useEffect(() => {
 
     fetchProduts().then((res) =>{
 
       setProducts(res);
+      setloading(false);
     });
     
   }, []);
   console.log(products);
 
   return(
-    <section className="products container">
-      {
-        products.map((product) =>
-          <ProductsCard key={product.id} data={{product}} />
-        )
-      }
-    </section>
+    (loading && <Loading />) || (
+      <section className="products container">
+        {
+          products.map((product) =>
+            <ProductsCard key={product.id} data={{product}} />
+          )
+        }
+      </section>
+    )
   );
 }
 
