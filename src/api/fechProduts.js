8fa27d573cde2b5e21b2c38e28a5dd880e84a824
Legatario <l.phillipe.d@gmail.com>
2024-05-16 17:26:30 -0300

    removendo comentarios

--- src/api/fechProduts.js
+++ src/api/fechProduts.js
@@ -1,5 +1,6 @@
 const fetchProduts = async () =>{
 
+  // const res = await fetch('https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=50&sortBy=id&orderBy=ASC');
   const res = await fetch('https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=50&sortBy=id&orderBy=ASC');
   const data = await res.json();
 
